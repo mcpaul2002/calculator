2f99c964ae8d4a482329784725e1b609275cf1f6
mc paul jr <msoriano@deakin.edu.au>
2020-09-25 12:13:54 +1000

    added server localhost

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const express = require ('express');
 const moragan = require ('morgan');
+
+
+app.listen(3000,  () =>{     
+    console.log('Web server running at: http://localhost:3000');     
+    console.log('Press Ctrl+C to shut down the web server'); 
+});
